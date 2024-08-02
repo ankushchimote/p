3e31379d647eb17264945f4384beb4c75d92fb96
Rahul Chimote <87609784+ankushchimote@users.noreply.github.com>
2024-08-02 21:25:26 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,6 +17,15 @@ export const About = () => {
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
             <div className={styles.aboutItemText}>
+              <h3>Engineer</h3>
+              <p>
+                I'm a 2024 graduate from Government College Of Engineering Amravati.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
               <h3>Frontend Developer</h3>
               <p>
                 I'm a frontend developer with experience in building responsive
@@ -25,21 +34,11 @@ export const About = () => {
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
             <div className={styles.aboutItemText}>
               <h3>Backend Developer</h3>
               <p>
                 I have basic knowledge of Java, C, C++, OOP and DSA.
-              </p>
-            </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
-            <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
-              <p>
-                I have designed multiple landing pages and have created design
-                systems as well
               </p>
             </div>
           </li>
