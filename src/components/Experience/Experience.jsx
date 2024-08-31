f4b14cfe748e5ab11bdccfe27e90f0d275d26845
Rahul Chimote <87609784+ankushchimote@users.noreply.github.com>
2024-08-31 10:15:18 +0530

    Update Experience.jsx

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -32,7 +32,7 @@ export const Experience = () => {
                 />
                 <div className={styles.historyItemDetails}>
                   <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
-                  <p>{`${historyItem.startDate}`} - Present</p>
+                  <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
                   <ul>
                     {historyItem.experiences.map((experience, id) => {
                       return <li key={id}>{experience}</li>;
