d7f8281f4bd7c92857c514aae8d648e012e48933
Rahul Chimote <87609784+ankushchimote@users.noreply.github.com>
2025-02-21 22:49:36 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,34 +1,20 @@
 import React from "react";
+
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
-  const handleDownload = () => {
-    const resumeUrl = "../../assets/Rahul_FullStack.pdf"; // Ensure resume.pdf is in the public folder
-    const link = document.createElement("a");
-    link.href = resumeUrl;
-    link.download = "Rahul_Chimote_Resume.pdf"; // Custom filename
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
   return (
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Rahul</h1>
         <p className={styles.description}>
-          I'm a FullStack developer with expertise in React, Spring Boot, and MERN.
-          Currently looking for an opportunity to kickstart my career.
+          I'm a FullStack developer with expertise in React,Springboot and MERN
+          . Currently looking for opportunity to kickstart my career.
         </p>
-        <div className={styles.buttonContainer}>
-          <a href="mailto:rahulchimote2001@gmail.com" className={styles.contactBtn}>
-            Contact Me
-          </a>
-          <button onClick={handleDownload} className={styles.contactBtn}>
-            Download Resume
-          </button>
-        </div>
+        <a href="mailto:rahulchimote2001@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
       </div>
       <img
         src={getImageUrl("hero/mee.webp")}
@@ -37,6 +23,6 @@ export const Hero = () => {
       />
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
-    </section> 
+    </section>
   );
 };
